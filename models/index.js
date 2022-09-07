0e58178ea6f419dcc2d14e1c0841b441b6ed151d
Noah Carralero <nole2kgames@gmail.com>
2022-09-07 00:28:05 -0400

    configured all models for sequelize

--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.hasMany(ProductTag)
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product)
 
 module.exports = {
   Product,
