f32f0d1fd1d346243875e48c6a959157bd26727e
Noah Carralero <nole2kgames@gmail.com>
2022-09-11 21:22:24 -0400

    fixed index model

--- models/index.js
+++ models/index.js
@@ -4,14 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category)
-// Categories have many Products
-Category.hasMany(Product)
-// Products belongToMany Tags (through ProductTag)
-Product.hasMany(ProductTag)
-// Tags belongToMany Products (through ProductTag)
-ProductTag.belongsToMany(Product)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
